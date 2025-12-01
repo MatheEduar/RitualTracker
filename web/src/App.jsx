9592c5dafe0fb0eb23f55d4de09d03e248e8203a
MatheEduar <edutheu_dev@hotmail.com>
2025-12-01 13:30:09 -0300

    chore(web): limpando o código inicial do vite

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+// Esse é o componente principal, a "Sala de Estar" da aplicação.
+
+export function App() {
+  return (
+    <div>
+      <h1>Ritual</h1>
+      <p>O seu habit tracker minimalista.</p>
+    </div>
+  )
+}
